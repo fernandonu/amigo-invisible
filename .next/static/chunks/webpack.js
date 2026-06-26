48794013b856c31edc9adeac4541531256827846
WIN-G6J2039MP1Q\Administrador <fernandonu@gmail.com>
2026-06-26 17:33:27 -0300

    arreglos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b16a8c6d9183930"; }
+/******/ 		__webpack_require__.h = function() { return "5309403ee3330165"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.75a7eda22297a4f9.hot-update.js
+++ .next/static/webpack/webpack.75a7eda22297a4f9.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "19ebd8815847d3b8"; }
+/******/ 	__webpack_require__.h = function() { return "5309403ee3330165"; }
 /******/ }();
 /******/ 
 /******/ }
